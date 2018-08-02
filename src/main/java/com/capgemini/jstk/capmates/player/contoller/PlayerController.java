b6afe5ee91a6ffd2b533d5482c0c58eef4a8b47b
Natalia Wojtkowiak <natalia.wojtkowiak@capgemini.com>
2018-08-02 22:43:25 +0200

    Add JavaDoc to PlayerController

--- src/main/java/com/capgemini/jstk/capmates/player/contoller/PlayerController.java
+++ src/main/java/com/capgemini/jstk/capmates/player/contoller/PlayerController.java
@@ -8,14 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 
 @RestController
@@ -28,11 +24,15 @@ public class PlayerController {
 
     @Autowired
     public PlayerController(PlayerService managePlayerProfile) {
-
         this.managePlayerProfile = managePlayerProfile;
-
     }
 
+    /**
+     * This method return dto with data of new player
+     *
+     * @param playerDTO - dto with data of new player
+     * @return filled dto supplemented by id
+     */
     @ResponseBody
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public PlayerDTO addNewPlayer(@RequestBody PlayerDTO playerDTO) {
@@ -46,6 +46,11 @@ public class PlayerController {
         return playerDTO;
     }
 
+    /**
+     * This method return list dto of all pleyers
+     *
+     * @return list dto of all players
+     */
     @RequestMapping(method = RequestMethod.GET)
     public List<PlayerDTO> showAllPlayer() {
 
@@ -53,21 +58,34 @@ public class PlayerController {
 
     }
 
+    /**
+     * This method return dto player or exception
+     *
+     * @param id - id of the player to be displayed
+     * @return - dto of the player with id from param id
+     * @throws ResourceNotFoundException is throw when player with given id doesn't exist
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public PlayerDTO showPlayerById(@PathVariable(value = "id") int id) throws ResourceNotFoundException{
+    public PlayerDTO showPlayerById(@PathVariable(value = "id") int id) throws ResourceNotFoundException {
         PlayerDTO playerDTO = managePlayerProfile.getPlayerInformation(id);
-        if(playerDTO != null){
-            return  playerDTO;
+        if (playerDTO != null) {
+            return playerDTO;
         }
         throw new ResourceNotFoundException();
 
     }
 
+    /**
+     * This method return players whose fitted to data given as parameters
+     * @param firstName  of player or part of it
+     * @param lastName of player or part of it
+     * @param motto of player or part of it
+     * @param email of player or part of it
+     * @return all players found
+     */
     @RequestMapping(value = "/search", method = RequestMethod.GET)
     public List<PlayerDTO> search(@RequestParam(name = "firstName", defaultValue = "") String firstName, @RequestParam(name = "lastName", defaultValue = "") String lastName,
                                   @RequestParam(name = "motto", defaultValue = "") String motto, @RequestParam(name = "email", defaultValue = "") String email) {
-
-        //@RequestBody PlayerDTO playerDTO) {
 
         PlayerDTO playerDTO = new PlayerDTO();
         playerDTO.setFirstName(firstName);
